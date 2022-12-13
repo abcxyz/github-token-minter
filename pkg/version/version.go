7debcb277a2f46d49e7a71537fb96e2cddfbd73e
Brad Egler <begler@gmail.com>
2022-12-13 13:10:53 -0500

    fumpt

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -17,13 +17,10 @@ package version
 var (
 	// Name is the name of the binary.
 	Name = "unknown"
-
 	// Version is the main package version.
 	Version = "unknown"
-
 	// Commit is the git sha.
 	Commit = "unknown"
-
 	// HumanVersion is the compiled version.
 	HumanVersion = Name + " v" + Version + " (" + Commit + ")"
 )
