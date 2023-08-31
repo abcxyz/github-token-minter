45253538febd8b67ece91a3044d29fada411de30
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2023-08-31 05:03:54 +0000

    Update infra template ref and inputs

--- abc.templates/infra/main.tf
+++ abc.templates/infra/main.tf
@@ -4,20 +4,13 @@ locals {
 }
 
 module "github_token_minter" {
-  source = "git::https://github.com/abcxyz/github-token-minter.git//terraform?ref=f1144811e6f6583b9aa971189ca8f6ef0f947c8a"
+  source = "git::https://github.com/abcxyz/github-token-minter.git//terraform?ref=ce04ede11817b4b73c1279f9dab5ee3d49c6f7d9"
 
   project_id = local.project_id
 
   domains    = ["REPLACE_DOMAINS"]
   dataset_id = "github_token_minter_audit_prod"
 
-  wif_id = "token-minter"
-  wif_github = {
-    owner_id   = "REPLACE_GITHUB_OWNER_ID"
-    owner_name = "REPLACE_GITHUB_OWNER_NAME"
-    repo_id    = "REPLACE_GITHUB_REPO_ID"
-    repo_name  = "REPLACE_GITHUB_REPO_NAME"
-  }
-  wif_attribute_condition   = "attribute.repository_owner_id == \"REPLACE_GITHUB_OWNER_ID\""
+  github_owner_id           = "REPLACE_GITHUB_OWNER_ID"
   ci_service_account_member = local.automation_service_account_member
 }
