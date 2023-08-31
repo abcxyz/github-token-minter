390187a01fc7e5510ed0c0ea675fa3811aac9f2e
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2023-08-31 04:48:22 +0000

    Combine backend.tf and terraform.tf

--- abc.templates/infra/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "REPLACE_BUCKET_NAME"
-    prefix = "REPLACE_BUCKET_PREFIX"
-  }
-}
--- abc.templates/infra/terraform.tf
+++ abc.templates/infra/terraform.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "gcs" {
+    bucket = "REPLACE_BUCKET_NAME"
+    prefix = "REPLACE_BUCKET_PREFIX"
+  }
+
   required_providers {
     google = {
       version = ">= 4.45"
