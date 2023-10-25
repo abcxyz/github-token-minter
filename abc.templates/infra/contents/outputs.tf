9a42fa77b68871cedcdab94e267f077109f82c3f
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2023-10-25 08:09:52 -0700

    chore: add custom_name input to support rendering of multiple instances of ghtm (#77)

--- abc.templates/infra/contents/outputs.tf
+++ abc.templates/infra/contents/outputs.tf
@@ -1,35 +1,35 @@
 output "gclb_external_ip_name" {
   description = "The external IPv4 name assigned to the global fowarding rule for the global load balancer."
-  value       = module.github_token_minter.gclb_external_ip_name
+  value       = module.REPLACE_MODULE_NAME.gclb_external_ip_name
 }
 
 output "gclb_external_ip_address" {
   description = "The external IPv4 assigned to the global fowarding rule for the global load balancer."
-  value       = module.github_token_minter.gclb_external_ip_address
+  value       = module.REPLACE_MODULE_NAME.gclb_external_ip_address
 }
 
 output "run_service_name" {
   description = "The Cloud Run service name."
-  value       = module.github_token_minter.run_service_name
+  value       = module.REPLACE_MODULE_NAME.run_service_name
 }
 
 output "run_service_url" {
   description = "The Cloud Run service url."
-  value       = module.github_token_minter.run_service_url
+  value       = module.REPLACE_MODULE_NAME.run_service_url
 }
 
 output "wif_pool" {
-  value = module.github_token_minter.wif_pool
+  value = module.REPLACE_MODULE_NAME.wif_pool
 }
 
 output "wif_provider" {
-  value = module.github_token_minter.wif_provider
+  value = module.REPLACE_MODULE_NAME.wif_provider
 }
 
 output "wif_service_account_email" {
-  value = module.github_token_minter.wif_service_account_email
+  value = module.REPLACE_MODULE_NAME.wif_service_account_email
 }
 
 output "wif_service_account_member" {
-  value = module.github_token_minter.wif_service_account_member
+  value = module.REPLACE_MODULE_NAME.wif_service_account_member
 }
