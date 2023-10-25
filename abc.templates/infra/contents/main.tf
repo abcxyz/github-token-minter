70f77365048e9c509afe3f967135ceda5f12da78
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2023-10-25 14:21:41 +0000

    Add custom name to abc.templates

--- abc.templates/infra/contents/main.tf
+++ abc.templates/infra/contents/main.tf
@@ -3,13 +3,13 @@ locals {
   automation_service_account_member = "REPLACE_AUTOMATION_SERVICE_ACCOUNT_MEMBER"
 }
 
-module "github_token_minter" {
+module "REPLACE_MODULE_NAME" {
   source = "git::https://github.com/abcxyz/github-token-minter.git//terraform?ref=v0.0.14"
 
   project_id = local.project_id
 
   domains    = ["REPLACE_DOMAIN"]
-  dataset_id = "github_token_minter_audit_prod"
+  dataset_id = "REPLACE_MODULE_NAME_audit_prod"
 
   github_owner_id           = "REPLACE_GITHUB_OWNER_ID"
   ci_service_account_member = local.automation_service_account_member
